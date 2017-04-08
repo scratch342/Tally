97483180b51a6357cb80db48db52affb4399bf97
Benny <blaze349@protonmail.com>
2017-04-08 08:27:30 +0000

    Allowed for dynamic amount values

--- Tally/src/app/tallies/tallies.component.ts
+++ Tally/src/app/tallies/tallies.component.ts
@@ -16,32 +16,12 @@ tallies: Tally[] = [];
 newTallyName: string;
 newTallyAmount: number;
 newTallyQuantity: number;
-private rice = 200;
-private riceAmount: number;
-private soup = 350;
-private soupAmount: number;
-private gari = 100;
-private gariAmount: number;
-private calculation: number;
 
 onAddTally(){
-    if(this.newTallyAmount == null && this.newTallyName === "Rice" || this.newTallyName === "rice" || this.newTallyName === "RICE"){
-    this.riceAmount = this.rice * this.newTallyQuantity;
-    this.newTallyAmount = this.riceAmount;
-    }
-  if(this.newTallyAmount == null && this.newTallyName === "Soup" || this.newTallyName === "soup" || this.newTallyName === "SOUP"){
-    this.soupAmount = this.soup * this.newTallyQuantity;
-    this.newTallyAmount = this.soupAmount;
-  }
-  if(this.newTallyAmount == null && this.newTallyName === "Gari" || this.newTallyName === "gari" || this.newTallyName ==="GARI"){
-    this.gariAmount = this.gari * this.newTallyQuantity;
-    this.newTallyAmount = this.gariAmount;
-  }
   this.tallies.push({tallyName: this.newTallyName, tallyAmount: this.newTallyAmount, tallyQuantity: this.newTallyQuantity})
   this.newTallyName = '';
   this.newTallyQuantity = null;
   this.newTallyAmount = null;
-  
   
 }
 onDeleteTally(tally){
