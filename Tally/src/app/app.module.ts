0fe02442070e3965500d04c9e1eb0da4bbcce640
Benny <blaze349@protonmail.com>
2017-04-08 10:30:02 +0000

    ,

--- Tally/src/app/app.module.ts
+++ Tally/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,8 @@ import { TallyService } from './tally.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [TallyService],
   bootstrap: [AppComponent]
